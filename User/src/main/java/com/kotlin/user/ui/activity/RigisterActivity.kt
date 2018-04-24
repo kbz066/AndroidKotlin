a193ca91f7195879576aa40979cd9c0ab5d11bd6
kbz <1529666546@qq.com>
2018-04-24 23:57:08 +0800

    配置模块化

---
+++ User/src/main/java/com/kotlin/user/ui/activity/RigisterActivity.kt
@@ -0,0 +1,18 @@
+package com.kotlin.user.ui.activity
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import com.kotlin.user.R
+
+import kotlinx.android.synthetic.main.activity_rigister.*
+
+class RigisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rigister)
+
+    }
+
+}
